0a420ce79e8282dae0b7824b8154d1890aa91f26
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-03-10 20:22:15 +0700

    code
    reformat.

--- u_dm.pas
+++ u_dm.pas
@@ -3,9 +3,9 @@ unit u_dm;
 interface
 
 uses
-  SysUtils, Classes, sSkinManager, DB, mySQLDbTables,dialogs,forms,inifiles,
-  frxClass, frxDBSet, ScktComp, frxDesgn, ImgList, Controls,
-  acAlphaImageList;
+  SysUtils, Classes, sSkinManager, DB, mySQLDbTables, dialogs, forms, inifiles,
+  frxClass, frxDBSet, ScktComp, frxDesgn, ImgList, Controls, acAlphaImageList,
+  SHFolder;
 
 type
   Tdm = class(TDataModule)
@@ -29,8 +29,8 @@ type
     gambar: TsAlphaImageList;
     procedure DataModuleCreate(Sender: TObject);
     procedure smAfterChange(Sender: TObject);
-    function FRObject(FastReport: TfrxReport; ObjectName: String): TObject;
-    function FRMemo (FastReport: TfrxReport; ObjectName: String): TfrxMemoView;
+    function FRObject(FastReport: TfrxReport; ObjectName: string): TObject;
+    function FRMemo(FastReport: TfrxReport; ObjectName: string): TfrxMemoView;
   private
     { Private declarations }
   public
@@ -39,18 +39,19 @@ type
 
 var
   dm: Tdm;
-  kd_comp,ip_kasir,macam_harga:string;
-  a_path,file_ini,cek_pusat:string;
-  sop:Boolean;
+  kd_comp, ip_kasir, macam_harga: string;
+  a_path, file_ini, cek_pusat: string;
+  sop: Boolean;
 
 implementation
 
-uses u_trans;
+uses
+  u_trans;
 
 
 {$R *.dfm}
 
-function krupuk(const s: String; CryptInt: Integer): String;
+function krupuk(const s: string; CryptInt: Integer): string;
 var
   i: integer;
   s2: string;
@@ -63,86 +64,88 @@ end;
 
 procedure Tdm.DataModuleCreate(Sender: TObject);
 var
-  data,pusat,jalur1,jalur2,nama,kata: string;
+  data, pusat, jalur1, jalur2, nama, kata: string;
   X: TextFile;
-  appINI : TIniFile;
+  appINI: TIniFile;
 begin
-  a_path:= extractfilepath(application.ExeName);
-  sm.SkinDirectory:=a_path+'\tools\skins';
+  a_path := extractfilepath(application.ExeName);
+  sm.SkinDirectory := a_path + '\tools\skins';
 
-  file_ini:= a_path+'\tools\gain.ini';
-  
-  appINI := TIniFile.Create(file_ini) ;
+  file_ini := a_path + '\tools\gain.ini';
+
+  appINI := TIniFile.Create(file_ini);
   try
-  kd_comp := appINI.ReadString ('kasir', 'kd_perusahaan', '');
+    kd_comp := appINI.ReadString('kasir', 'kd_perusahaan', '');
 
-  sm.SkinName:=appINI.ReadString('kasir','nama_skin','WEB (internal)');
-  sm.HueOffset:=appini.ReadInteger('kasir','hue_skin',0);
-  sm.Saturation:=appini.ReadInteger('kasir','sat_skin',0);
- finally
- appINI.Free;
- end;
+    sm.SkinName := appINI.ReadString('kasir', 'nama_skin', 'WEB (internal)');
+    sm.HueOffset := appini.ReadInteger('kasir', 'hue_skin', 0);
+    sm.Saturation := appini.ReadInteger('kasir', 'sat_skin', 0);
+  finally
+    appINI.Free;
+  end;
 
-dm.My_Conn.Connected:= False;
- assignfile(X,'tools\koneksi.cbCon');
- try
-   reset(X);
-   readln(X,pusat);
-   readln(X,data);
-   readln(X,jalur2);
-   readln(X,nama);
-   readln(X,kata);
-   closefile(X);
- dm.my_conn.Host :=krupuk(pusat,6);
- dm.My_Conn.DatabaseName:= krupuk(data,6);
- jalur1 :=krupuk(jalur2,6);
- dm.my_conn.Port :=strtoint(jalur1);
- dm.my_conn.UserName :=krupuk(nama,6);
- dm.my_conn.UserPassword :=krupuk(kata,6);
- dm.My_Conn.Connected:= true;
- except
- showmessage('koneksi tidak berhasil');
- application.Terminate;
- end;
+  dm.My_Conn.Connected := False;
+  assignfile(X, 'tools\koneksi.cbCon');
+  try
+    reset(X);
+    readln(X, pusat);
+    readln(X, data);
+    readln(X, jalur2);
+    readln(X, nama);
+    readln(X, kata);
+    closefile(X);
+    dm.my_conn.Host := krupuk(pusat, 6);
+    dm.My_Conn.DatabaseName := krupuk(data, 6);
+    jalur1 := krupuk(jalur2, 6);
+    dm.my_conn.Port := strtoint(jalur1);
+    dm.my_conn.UserName := krupuk(nama, 6);
+    dm.my_conn.UserPassword := krupuk(kata, 6);
+    dm.My_Conn.Connected := true;
+  except
+    showmessage('koneksi tidak berhasil');
+    application.Terminate;
+  end;
 end;
 
 procedure Tdm.smAfterChange(Sender: TObject);
-  var appINI : TIniFile;
+var
+  appINI: TIniFile;
 begin
- appINI := TIniFile.Create(file_ini) ;
- try
-  appINI.WriteString('kasir','nama_skin',dm.sm.SkinName);
-  appINI.WriteInteger('kasir','hue_skin',dm.sm.HueOffset);
-  appINI.WriteInteger('kasir','sat_skin',dm.sm.Saturation);
- finally
- appINI.Free;
- end;
+  appINI := TIniFile.Create(file_ini);
+  try
+    appINI.WriteString('kasir', 'nama_skin', dm.sm.SkinName);
+    appINI.WriteInteger('kasir', 'hue_skin', dm.sm.HueOffset);
+    appINI.WriteInteger('kasir', 'sat_skin', dm.sm.Saturation);
+  finally
+    appINI.Free;
+  end;
 end;
 
-function Tdm.FRObject(FastReport: TfrxReport; ObjectName: String): TObject;
+function Tdm.FRObject(FastReport: TfrxReport; ObjectName: string): TObject;
 var
   i: Integer;
   AFound: Boolean;
 begin
-  AFound:=False;
+  AFound := False;
   for I := 0 to FastReport.ComponentCount - 1 do
   begin
     if LowerCase(FastReport.Components[i].Name) = LowerCase(ObjectName) then
     begin
-      Result:=TObject(FastReport.Components[i]);
-      AFound:=True;
+      Result := TObject(FastReport.Components[i]);
+      AFound := True;
       Break;
     end;
   end;
   if not AFound then
   begin
-    Result:=nil;
+    Result := nil;
   end;
 end;
- 
-function Tdm.FRMemo (FastReport: TfrxReport; ObjectName: String): TfrxMemoView;
+
+function Tdm.FRMemo(FastReport: TfrxReport; ObjectName: string): TfrxMemoView;
 begin
   Result := TfrxMemoView(FRObject(FastReport, ObjectName));
 end;
 
 end.
+
